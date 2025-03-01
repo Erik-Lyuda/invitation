e654d94662f7f47621f749a6a9fc22506fdb2b44
Arm313 <armbabujyan@gmail.com>
2025-03-02 00:24:54 +0400

    changed email

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const calculateTimeLeft = (targetDate) => {
-    const now = new Date().getTime();
-    const distance = new Date(targetDate).getTime() - now;
-  
-    if (distance <= 0) {
-      return {
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0,
-      };
-    }
-  
+  const now = new Date().getTime();
+  const distance = new Date(targetDate).getTime() - now;
+
+  if (distance <= 0) {
     return {
-      days: Math.floor(distance / (1000 * 60 * 60 * 24)),
-      hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-      minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
-      seconds: Math.floor((distance % (1000 * 60)) / 1000),
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
     };
+  }
+
+  return {
+    days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+    hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+    seconds: Math.floor((distance % (1000 * 60)) / 1000),
   };
+};
 
 const targetDate = "2025-05-31T15:00";
 
@@ -26,39 +26,54 @@ const hoursEl = document.getElementById("hours");
 const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
-
 const interval = setInterval(() => {
-    const timeLeft = calculateTimeLeft(targetDate);
-  
-    daysEl.textContent = String(timeLeft.days).padStart(2, "0");
-    hoursEl.textContent = String(timeLeft.hours).padStart(2, "0");
-    minutesEl.textContent = String(timeLeft.minutes).padStart(2, "0");
-    secondsEl.textContent = String(timeLeft.seconds).padStart(2, "0");
-  
-    if (timeLeft.days === 0 && timeLeft.hours === 0 && timeLeft.minutes === 0 && timeLeft.seconds === 0) {
-      clearInterval(interval);
-    }
-  }, 1000);
+  const timeLeft = calculateTimeLeft(targetDate);
 
-  document.addEventListener("DOMContentLoaded", function () {
-    emailjs.init("zUojn6FauoICty7qS"); 
+  daysEl.textContent = String(timeLeft.days).padStart(2, "0");
+  hoursEl.textContent = String(timeLeft.hours).padStart(2, "0");
+  minutesEl.textContent = String(timeLeft.minutes).padStart(2, "0");
+  secondsEl.textContent = String(timeLeft.seconds).padStart(2, "0");
 
-    document.getElementById("sendEmail").addEventListener("click", function () {
-      let guestName = document.querySelector('#guestName')
-      let yesOrNo = document.querySelector('input[name="yesno"]:checked').value
-        let templateParams = {
-            name: guestName.value,
-            message: `${yesOrNo === "yes" ? "Գալու ենք " : "չենք կարողանա գալ"}`
-        };
-        console.log(templateParams);
+  if (
+    timeLeft.days === 0 &&
+    timeLeft.hours === 0 &&
+    timeLeft.minutes === 0 &&
+    timeLeft.seconds === 0
+  ) {
+    clearInterval(interval);
+  }
+}, 1000);
 
-        emailjs.send("service_1ssajei", "template_koczw9e", templateParams)
-            .then(function (response) {
-                console.log("SUCCESS!", response.status, response.text);
-                alert("Նամակը հաջողությամբ ուղարկվեց!");
-            }, function (error) {
-                console.log("FAILED...", error);
-                alert("Սխալ տեղի ունեցավ!");
-            });
+document.addEventListener("DOMContentLoaded", function () {
+  emailjs.init("zUojn6FauoICty7qS");
+
+  document.getElementById("sendEmail").addEventListener("click", function () {
+    let guestName = document.querySelector("#guestName");
+    let yesOrNo = document.querySelector('input[name="yesno"]:checked');
+    yesOrNo.checked = false;
+    let templateParams = {
+      name: guestName.value,
+      message: `${
+        yesOrNo.value === "yes" ? "Գալու ենք " : "չենք կարողանա գալ"
+      }`,
+    };
+    console.log(templateParams);
+    guestName.value = "";
+    yesOrNo.checked = false;
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
     });
+
+    emailjs.send("service_1ssajei", "template_koczw9e", templateParams).then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("հաջողությամբ հաստատվեց!");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        // alert("Սխալ տեղի ունեցավ!");
+      }
+    );
+  });
 });
